0ca01f75270bb0a1ddb4955f9c43e000b5b3343c
Alexander Potts <alex.potts@edg.com.au>
2023-03-19 00:06:22 +1100

    First run of service creation

--- 05-serving/src/predict.py
+++ 05-serving/src/predict.py
@@ -1,36 +1,38 @@
 import pickle
 
+import numpy as np
+from flask import Flask, request, jsonify, Response
 
 INPUT_PATH = "../models/model_reg=%s.bin"
 final_reg_factor: float = 0.1
+
 # Time to load!
 # Note: This requires scikit-learn to be installed
 with open(INPUT_PATH % final_reg_factor, "rb") as file_model_input:
     (loaded_vect, loaded_model) = pickle.load(file_model_input)
 
-test_customer = dict(
-    gender="female",
-    seniorcitizen=0,
-    partner="yes",
-    dependents="no",
-    phoneservice="no",
-    multiplelines="no_phone_service",
-    internetservice="dsl",
-    onlinesecurity="no",
-    onlinebackup="yes",
-    deviceprotection="no",
-    techsupport="no",
-    streamingtv="no",
-    streamingmovies="no",
-    contract="month-to-month",
-    paperlessbilling="yes",
-    paymentmethod="electronic_check",
-    tenure="1",
-    monthlycharges=29.85,
-    totalcharges=29.85
-)
+
+app = Flask("churn")
 
 
-X = loaded_vect.transform([test_customer])
-prediction = loaded_model.predict_proba(X)[0, 1]
-print(prediction)
+def model_prediction(customer: dict) -> tuple[float, float]:
+    X: np.ndarray = loaded_vect.transform([customer])
+    prediction: float = loaded_model.predict_proba(X)[0, 1]
+    churn: float = prediction >= 0.5
+
+    return prediction, churn
+
+
+@app.route("/predict", methods=["POST"])
+def predict() -> Response:
+    customer: dict = request.get_json()
+
+    prediction, churn = model_prediction(customer=customer)
+
+    response = dict(churn_probability=prediction, churn=churn)
+
+    return jsonify(response)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
