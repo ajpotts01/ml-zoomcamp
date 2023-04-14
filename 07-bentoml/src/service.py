44fefd2cafe01a7aed39252c7eb87fba81320070
Alexander Potts <alex.potts@edg.com.au>
2023-04-15 00:06:59 +1000

    Ch7 progress - BentoML basic example

--- 07-bentoml/src/service.py
+++ 07-bentoml/src/service.py
@@ -1,7 +1,40 @@
 # Non-standard libs
 import bentoml
-from bentoml.io import JSON
+from bentoml.io import JSON, NumpyNdarray
 from bentoml import Model, Runner, Service
+from pydantic import BaseModel
+
+# {
+#   "seniority": 7,
+#   "home": "other",
+#   "time": 48,
+#   "age": 34,
+#   "marital": "married",
+#   "records": "no",
+#   "job": "freelance",
+#   "expenses": 45,
+#   "income": 37.0,
+#   "assets": 0.0,
+#   "debt": 0.0,
+#   "amount": 800,
+#   "price": 941
+# }
+
+class CreditApplication(BaseModel):
+  seniority: int
+  home: str
+  time: int
+  age: int
+  marital: str
+  records: str
+  job: str
+  expenses: int
+  income: float
+  assets: float
+  debt: float
+  amount: int
+  price: int
+
 
 CURRENT_MODEL_TAG = "credit_risk_model:latest"
 
@@ -16,8 +49,9 @@ model_service: Service = bentoml.Service(
 )
 
 
-@model_service.api(input=JSON(), output=JSON())
-def classify(application_data):
+@model_service.api(input=JSON(pydantic_model=CreditApplication), output=JSON())
+def classify(credit_application: CreditApplication):
+    application_data = credit_application.dict()
     vector = dv.transform(application_data)
 
     model_prediction = model_runner.predict.run(vector)
